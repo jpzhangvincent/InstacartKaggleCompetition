7182be3534255256bd6e776d3fb7a6bedcc03717
Jeremy <jeweidner@ucdavis.edu>
2017-06-12 17:45:04 -0700

    corrected .fit command

--- models/OvR/ovrmodel.py
+++ models/OvR/ovrmodel.py
@@ -12,10 +12,11 @@ ytrain = pd.read_pickle(open('y_train.pickle', 'rb'))
 xtrain = xtrain.iloc[:,-100:]
 xtrain = xtrain.values
 
-#model.fit(xtrain, ytrain)
+
 
 def main():
 	model = OneVsRestClassifier(LinearSVC(random_state=1), n_jobs=-1)
+	model.fit(xtrain, ytrain)
 
 	filename = 'OvRmodel.sav'
 	pickle.dump(model, open(filename, 'wb'))
